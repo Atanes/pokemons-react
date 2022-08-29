dbe1e2b72fe0d5fe7aae335b4b2459463493acd7
Atanes <aatanes100@gmail.com>
2022-08-29 11:47:24 -0300

    Inclusão de mais informações e ajuste no layout dos cards

--- src/components/CardPokemon/CardPokemon.tsx
+++ src/components/CardPokemon/CardPokemon.tsx
@@ -11,6 +11,7 @@ declare interface Pokemon {
   id: string | undefined,
   nome: string | undefined,
   imagen: string | undefined,
+  habilidades: Array<string> | [],
 }
 
 const CardPokemon: React.FC<CardPokemonProps> = (props) => {
@@ -18,7 +19,9 @@ const CardPokemon: React.FC<CardPokemonProps> = (props) => {
   useEffect(() => {
     getPokemon(props.nomePokemon)
       .then((dados) => {
-        setPokemon({ id: dados.id, nome: dados.name, imagen: dados.sprites.front_default })
+        const nome = dados.name.charAt(0).toLocaleUpperCase() + dados.name.slice(1);
+        const _habilidades = dados.abilities.map((habilidade: { ability: { name: string; }; })  => habilidade.ability.name);
+        setPokemon({ id: dados.id, nome: nome, imagen: dados.sprites.front_default, habilidades: _habilidades })
       })
       .catch((err) => "Ocorreu um erro no processamento");
   }, [props.nomePokemon]);
@@ -28,6 +31,7 @@ const CardPokemon: React.FC<CardPokemonProps> = (props) => {
       <div className="card-pokemon">
         <span>Id: {pokemon?.id}</span>
         <span>Nome: {pokemon?.nome}</span>
+        <span>Habilidades: {pokemon?.habilidades.join(', ')}</span>
       </div>
       <img src={pokemon?.imagen} alt={pokemon?.nome} />
     </section>
