1dab90c6d25e871444eedc2a284f75f19dd3d072
Atanes <aatanes100@gmail.com>
2022-08-31 11:40:55 -0300

    Correção do action e do reducer do Pokemon

--- src/redux/Pokemons/Pokemons.actions.ts
+++ src/redux/Pokemons/Pokemons.actions.ts
@@ -1,16 +1,10 @@
 import { Action } from "./Pokemons.reducer";
-import { Pokemon } from "../../components/CardPokemon/CardPokemon";
 
 const INSERT_NEW_POKEMON = 'INSERT_NEW_POKEMON';
 
-export const insertNewPokemon = (): Action<Pokemon> => {
+export const insertNewPokemon = (payload: string ): Action<string> => {
     return {
         type: INSERT_NEW_POKEMON,
-        payload: {
-            id: '1',
-            nome: 'bulbasaur',
-            imagen: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-            habilidades: ['overgrow','chlorophyll']
-        }
+        payload
     }
 }
--- src/redux/Pokemons/Pokemons.reducer.tsx
+++ src/redux/Pokemons/Pokemons.reducer.tsx
@@ -1,3 +1,4 @@
+import arrayPokemons from "../../utils/dadosPokemons";
 /* eslint-disable import/no-anonymous-default-export */
 const INSERT_NEW_POKEMON = 'INSERT_NEW_POKEMON';
 
@@ -5,10 +6,11 @@ export interface Action<T = any> {
     type: string,
     payload?: T
 }
-export default function (state = [], action: Action) {
+export default function (state = arrayPokemons, action: Action) {
     switch (action.type) {
         case INSERT_NEW_POKEMON:
-            return [...state, action.payload]
+            if (!state.includes(action.payload)) state.push(action.payload)
+            return state
         default:
             return state
     }
