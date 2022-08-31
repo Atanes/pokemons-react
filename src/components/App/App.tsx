ffe95638642cf2957a03c362a10f358e1ccd1287
Atanes <aatanes100@gmail.com>
2022-08-31 10:21:00 -0300

    Incluindo um novo elemento com o Redux

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import CardPokemon from '../CardPokemon';
+import { useDispatch } from 'react-redux';
+import { insertNewPokemon } from '../../redux/Pokemons/Pokemons.actions';
 
 function App() {
-  const pokemons = ['pikachu', 'ditto', 'charmander', 'squirtle', 'caterpie', 'weedle', 'rattata']
+  const arrayPokemons = ['pikachu', 'ditto', 'charmander', 'squirtle', 'caterpie', 'weedle', 'rattata']
+  const [pokemons, setPokemons] = useState(listaPokemons());
+  const dispatch = useDispatch();
+  function novoPokemon() {
+    const novoPokemon = dispatch(insertNewPokemon()).payload;
+    arrayPokemons.push(novoPokemon?.nome || 'Atanes');
+    setPokemons(listaPokemons());
+  }
+
+  function listaPokemons() {
+    return arrayPokemons.map((pokemon) =>
+      <CardPokemon key={pokemon} nomePokemon={pokemon} />)
+  }
+
   return (
     <div className="App" >
       <h1>Listagem de Pokemons - React </h1>
       <section className='listagem'>
-      {pokemons.map((pokemon) =>
-        <CardPokemon key={pokemon} nomePokemon={pokemon} />)}
+        {pokemons}
       </section>
+      <button onClick={novoPokemon}>Novo Pokemon</button>
     </div>
   );
 }
 
 export default App;
+
+
--- src/redux/Pokemons/Pokemons.reducer.tsx
+++ src/redux/Pokemons/Pokemons.reducer.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+const INSERT_NEW_POKEMON = 'INSERT_NEW_POKEMON';
+
 export interface Action<T = any> {
     type: string,
     payload?: T
 }
 export default function (state = [], action: Action) {
     switch (action.type) {
+        case INSERT_NEW_POKEMON:
+            return [...state, action.payload]
         default:
             return state
     }
